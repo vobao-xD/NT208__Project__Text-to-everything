4f2f27a9867132fa5668c988eda2227bb8aab742
antss50 <tangocanss50@gmail.com>
2025-05-30 21:20:30 +0700

    complete chat history

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -68,6 +68,11 @@ class ChatDetail(Base):
     input_type = Column(String, nullable=False, index=True)
     text_prompt = Column(String, nullable=True)
     input_file_name = Column(String, nullable=True)
+
+    output_type = Column(String, nullable=True)
+    output_text = Column(Text, nullable=True)
+    output_url = Column(String, nullable=True)
+
     created_at = Column(DateTime, default=func.now())
     generator = relationship("Generator")
 
