88cc4dcce0a1cd3cdbb714280e821722608b81d1
vobao-xD <vobao8682@gmail.com>
2025-06-09 16:14:48 +0000

    temp commit

--- backend/app/api/image_enhance.py
+++ backend/app/api/image_enhance.py
@@ -1,34 +1,58 @@
 from fastapi import FastAPI, Depends, File, HTTPException,APIRouter, Request, UploadFile
 from PIL import Image, ImageEnhance
 from fastapi.responses import StreamingResponse
+from requests import Session
+from db.database import get_db
+from services.authentication_and_authorization import verify_user_access_token
+from services import OutputManager
 import io
 
 router=APIRouter()
 
 @router.post('/enhance')
-async def enhance_image(file: UploadFile=File(...)):
+async def enhance_image(request: Request, file: UploadFile=File(...), db: Session = Depends(get_db)):
+    user_data = verify_user_access_token(source="cookie", request=request)
     if not file.content_type.startswith('image/'):
         raise HTTPException(status_code=400,detail="File phải là hình ảnh")
     try:
-        image_data=await file.read()
-        image=Image.open(io.BytesIO(image_data)).convert('RGB')
+
+        image_data = await file.read()
+        image = Image.open(io.BytesIO(image_data)).convert('RGB')
 
         #1 Cải thiện độ sáng
-        enhancer=ImageEnhance.Sharpness(image)
-        image=enhancer.enhance(2.0)
+        enhancer = ImageEnhance.Sharpness(image)
+        image = enhancer.enhance(2.0)
 
         #2 Điều chỉnh độ sáng
-        enhancer=ImageEnhance.Brightness(image)
-        image=enhancer.enhance(1.1)
+        enhancer = ImageEnhance.Brightness(image)
+        image = enhancer.enhance(1.1)
 
         #3 Điều chỉnh độ tương phản
-        enhancer=ImageEnhance.Contrast(image)
-        image=enchane_image=enhancer.enhance(1.2)
+        enhancer = ImageEnhance.Contrast(image)
+        image = enchane_image=enhancer.enhance(1.2)
 
         #Lưu hình ảnh
         output_buffer=io.BytesIO()
-        enchane_image.save(output_buffer,format="JPEG",quality=95)
+        enchane_image.save(output_buffer, format="JPEG", quality=95)
         output_buffer.seek(0)
+
+        save_path = OutputManager.save_output_file(
+                    user_email=user_data["email"],
+                    generator_name="image-quality-enhancing",
+                    file_content=output_buffer,
+                    file_extension="jpeg"
+                )
+        
+        OutputManager.log_chat(
+                    db=db,
+                    user_email=user_data["email"],
+                    generator_name="image-quality-enhancing",
+                    input_type="",
+                    text_prompt="improve this image quality",
+                    output_type="audio",
+                    output_file_path=str(save_path)
+                )
+        
 
         #Trả về hình ảnh
         return StreamingResponse(
--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -15,25 +15,25 @@ Base = declarative_base()
 session = SessionLocal()
 
 # Danh sách generator cần chèn
-generators_to_insert = [
-    {"name": "text-to-speech-default", "input_type": "text", "output_type": "audio"},
-    {"name": "text-to-speech-custom", "input_type": "text", "output_type": "audio"},
-    {"name": "text-to-image", "input_type": "text", "output_type": "image"},
-    {"name": "text-to-video", "input_type": "text", "output_type": "video"},
-    {"name": "text-to-code", "input_type": "text", "output_type": "code"}
-]
+# generators_to_insert = [
+#     {"name": "text-to-speech-default", "input_type": "text", "output_type": "audio"},
+#     {"name": "text-to-speech-custom", "input_type": "text", "output_type": "audio"},
+#     {"name": "text-to-image", "input_type": "text", "output_type": "image"},
+#     {"name": "text-to-video", "input_type": "text", "output_type": "video"},
+#     {"name": "text-to-code", "input_type": "text", "output_type": "code"}
+# ]
 
-try:
-    for gen in generators_to_insert:
-        exists = session.query(Generator).filter_by(name=gen["name"]).first()
-        if not exists:
-            session.add(Generator(**gen))
-    session.commit()
-except IntegrityError:
-    session.rollback()
-    print("Dữ liệu đã tồn tại, rollback.")
-finally:
-    session.close()
+# try:
+#     for gen in generators_to_insert:
+#         exists = session.query(Generator).filter_by(name=gen["name"]).first()
+#         if not exists:
+#             session.add(Generator(**gen))
+#     session.commit()
+# except IntegrityError:
+#     session.rollback()
+#     print("Dữ liệu đã tồn tại, rollback.")
+# finally:
+#     session.close()
     
 def get_db():
     db = SessionLocal()
